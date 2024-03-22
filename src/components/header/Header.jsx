014cfdfe5fafbc773b9cecef95f6b24c14597699
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-03-22 18:10:57 +0530

    fixed header file and made few changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">Jot Down Your Ideas <hr/> with</span>
-        <span className="headerTitleLg">Jottr</span>
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">BLOG</span>
       </div>
       <img
         className="headerImg"
-        src="https://images.unsplash.com/photo-1496307653780-42ee777d4833?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWluaW1hbCUyMGJsYWNrJTIwYW5kJTIwd2hpdGUlMjA0a3xlbnwwfDB8MHx8fDA%3D"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt=""
       />
     </div>
