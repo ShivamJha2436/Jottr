b3ea74e0c27dfa373e0d66fe1832e07b15331d26
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-07-06 21:40:34 +0530

    Rebasing the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className='justify-center ml-52 mt-4 font-extrabold '>Hello World</h1>
     </div>
   )
 }
